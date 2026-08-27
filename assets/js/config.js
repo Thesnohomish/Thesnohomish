606c882a11ac09660fa33af14ea115cf435fbb5d
Thesnohomish <snohomishkenya@gmail.com>
2026-08-27 10:17:05 +0300

    Update delivery phone numbers in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-window.CHUPAHUB_CONFIG = { currency: 'KSh', deliveryPhone: '0700 000 000', supabaseUrl: '', supabaseAnonKey: '' };
+window.CHUPAHUB_CONFIG = { currency: 'KSh', deliveryPhone: '0700 000 000', '0100 000 000', '+254 000 000', supabaseUrl: '', supabaseAnonKey: '' };
